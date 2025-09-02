41f9a7d875eab43a026f8f68462afda3d95af53e
keerthivasan91 <keerthivasan1617@gmail.com>
2025-09-02 21:57:52 +0530

    refactor contacts.py for improved code readability and organization

--- contacts.py
+++ contacts.py
@@ -73,4 +73,4 @@ def update_contact(id):
     return render_template('update.html', contact=contact)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
